8e41b9272cf2b5017b7a9e4121f4b9ed6303d7ec
ivandmitrienko <oneblackcrystal@gmail.com>
2021-10-01 14:35:02 +0300

    initial comit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-//here will by JavaScript code ...
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    centeredSlides: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next", 
+      prevEl: ".swiper-button-prev",
+    },
+  });

9180fa63a8a281a701b6333d470cc9865a8f0277
ivandmitrienko <oneblackcrystal@gmail.com>
2021-10-10 23:21:35 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
-var swiper = new Swiper(".mySwiper", {
+new Swiper(".mySwiper", {
     slidesPerView: 6,
     centeredSlides: true,
     spaceBetween: 18,
+    navigation: {
+      nextEl: ".swiper-button-next", 
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 0,
     navigation: {
       nextEl: ".swiper-button-next", 
       prevEl: ".swiper-button-prev",

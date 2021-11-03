f8716969555220d59da54f30c76a0b105fabe7a2
ivandmitrienko <oneblackcrystal@gmail.com>
2021-11-03 23:49:50 +0300

    initial commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,3 +3,9 @@ $(document).ready(function() {
         $('.main-sidebar__burger,.main-sidebar-menu').toggleClass('active');
     });
 });
+
+$(document).ready(function() {
+    $('.blog-content__burger').click(function(event) {
+        $('.blog-content__burger,.blogs-sidebar').toggleClass('active');
+    });
+});

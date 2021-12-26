71c6fdfa5834271eaf3b3ec0156cfe345fe7ab0c
ivandmitrienko <oneblackcrystal@gmail.com>
2021-12-27 01:18:22 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,45 @@
-"use strict"
+// "use strict"
 
-function newAnketa() {
+// function newAnketa() {
 
-    let name, patronymic, surname, age, gender, retired;
+//     let name, patronymic, surname, age, gender, retired;
 
-    do{
-        surname = prompt("Введите Вашу фамилию", ""); 
-    } while (parseFloat(surname) || surname === null || surname === "");
+//     do{
+//         surname = prompt("Введите Вашу фамилию", ""); 
+//     } while (parseFloat(surname) || surname === null || surname === "");
 
-    do{
-        name = prompt("Введите Ваше имя", ""); 
-    } while (parseFloat(name) || name === null || name === "");
+//     do{
+//         name = prompt("Введите Ваше имя", ""); 
+//     } while (parseFloat(name) || name === null || name === "");
 
-    do{
-        patronymic = prompt("Введите Вашу отчество", ""); 
-    } while (parseFloat(patronymic) || patronymic === null || patronymic === "");
+//     do{
+//         patronymic = prompt("Введите Вашу отчество", ""); 
+//     } while (parseFloat(patronymic) || patronymic === null || patronymic === "");
 
-    do{
-        age = +prompt("Введите Ваш возвраст", ""); 
-    } while (!isFinite(age) || age === null || age === "");
+//     do{
+//         age = +prompt("Введите Ваш возвраст", ""); 
+//     } while (!isFinite(age) || age === null || age === "");
 
-    gender = confirm("Ваш пол мужской?")? "мужской" : "женский";
+//     gender = confirm("Ваш пол мужской?") ? "мужской" : "женский";
 
-//      if (gender = "мужской" &&  age > 63) 
-//         retired = "да";
-//          else 
-//            retired = "нет"; 
+// //      if (gender = "мужской" &&  age > 63) 
+// //         retired = "да";
+// //          else 
+// //            retired = "нет"; 
         
 
 
 
 
-    alert(`ваше ФИО: ${surname} ${name} ${patronymic}
-ваш возраст в годах: ${age}
-ваш возраст в днях: ${age * 365}
-через 5 лет вам будет: ${age + 5}
-ваш пол: ${gender}
-вы на пенсии: ${retired}`);
-}
+//     alert(`ваше ФИО: ${surname} ${name} ${patronymic}
+// ваш возраст в годах: ${age}
+// ваш возраст в днях: ${age * 365}
+// через 5 лет вам будет: ${age + 5}
+// ваш пол: ${gender}
+// вы на пенсии: ${retired}`);
+// }
 
-newAnketa();
+// newAnketa();
 
 // let x = parseInt('a123');
 // console.log( Object.is(parseInt('a123131'), NaN) );

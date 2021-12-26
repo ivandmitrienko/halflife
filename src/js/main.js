6f12bdbb4f8582772a4fa52cc20c13a02decb99a
ivandmitrienko <oneblackcrystal@gmail.com>
2021-12-27 00:02:14 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
-function ANKETA() {
-    
-}
+// "use strict"
+
+// function newAnketa() {
+
+//     let name;
+
+//     do{
+//         name = prompt("Введите Ваше имя", ""); 
+//     } while (parseFloat(name) || name === null || name === "");
+
+//     alert(`ваше ФИО: Иванов ${name} Иванович
+// ваш возраст в годах: 20
+// ваш возраст в днях: 7300
+// через 5 лет вам будет: 25
+// ваш пол: мужской
+// вы на пенсии: нет`);
+// }
+
+// newAnketa();
+
+
+// do {
+//     num = prompt("Введите число", 0);
+// } while ( !isFinite(num) || num === null || num === '' );
+// patronymic, surname, age, ageInDays, ageInFiveYears, gender, retired; 
